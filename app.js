fc06f97a42c905a5c811be3279cfda3c84237d0c
Tayfun Bektaş <63612384+tayfunbkts@users.noreply.github.com>
2022-10-24 16:28:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const result = document.getElementById("result");
-const ask = confirm('🍻 Bira İçelim mi? 🍻')
+// const ask = confirm('🍻 Bira İçelim mi? 🍻')
+const evet = document.getElementById('evet')
+const hayir = document.getElementById('hayir')
+let anket;
 
-if(ask){
+evet.addEventListener('click', function(){
+    anket = true;
+    if(anket){
     
-    result.innerHTML = `<div class="buttonHolder">
-    <button class="button tick" id="sali"></button>
-    <button class="button cross" id="cars"></button>
-  </div>`
-}else{
-    result.innerHTML = `<h1>🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻</h1> <br>
-    <h1>Bunu duyduğuma üzüldüm</h1>`
-}
-
-const buttonSal = document.getElementById('sali')
+        result.innerHTML = `<div class="buttonHolder">
+        <button class="button tick" id="sali"></button>
+        <button class="button cross" id="cars"></button>
+      </div>`
+    }else{
+        result.innerHTML = `<h1>🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻</h1> <br>
+        <h1>Bunu duyduğuma üzüldüm</h1>`
+    }
+    const buttonSal = document.getElementById('sali')
 const buttonCar = document.getElementById('cars')
 
 buttonSal.addEventListener('click', function(){
@@ -26,4 +30,13 @@ buttonCar.addEventListener('click', function(){
     <h1> Tamamdır Çarşamba günü görüşüyoruz
   </div>`
 })
+
+})
+
+hayir.addEventListener('click', function(){
+    result.innerHTML = `<h1>🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻🍻</h1> <br>
+        <h1>Bunu duyduğuma üzüldüm</h1>`
+    })
+
+
 
